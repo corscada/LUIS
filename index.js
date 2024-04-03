dd32003fbd164653ef4df70594d4846e6c2203c3
corsada <james@corscada.uk>
2024-04-03 13:40:39 +0100

    adds global average default to calculation

--- index.js
+++ index.js
@@ -1,15 +1,16 @@
 const LandUsage = (globalConfig) => {
-  console.log(globalConfig)
 
   const metadata = {
     kind: 'execute',
   };
 
+  const landUse = 0.0000018330936073059; //global average defualt
+
   const execute = async (inputs, config) => {
 
     return inputs.map(input => {
       const energy = input['cpu/energy'];
-      const outputValue = energy * 10;
+      const outputValue = energy * landUse;
 
       return {
         ...input,
